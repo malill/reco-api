2fff4419766b99c0b8f32524d5d9a9698aee476d
malill <mark.lill@freenet.de>
2021-09-04 10:17:47 +0200

    feat: create settings file

---
+++ core/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    ENVIRONMENT: str
+
+    class Config:
+        env_file_encoding = 'utf-8'
+
+
+load_dotenv()
+settings = Settings(_env_file='core/envs/dev.env',
+                    _env_file_encoding='utf-8')
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from v1.api import api_router
 
-app = FastAPI(title="Reco API")
+from core.config import settings
 
-app.include_router(api_router, prefix="/v1")
+version = settings.API_V1_STR
+
+app = FastAPI(title="Recommendation API",
+              version=0.1,
+              openapi_url=f"{version}/openapi.json",
+              description="REST API that exposes calculated recommendations from Recommender Builder")
+
+app.include_router(api_router, prefix=version)
+
+print(f"running app in {settings.ENVIRONMENT} mode ...")
