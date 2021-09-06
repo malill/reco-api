c7b56a37cb91eba7b5fb1da21cc1299a8998ac16
malill <markus.lill@gmx.de>
2021-09-06 15:52:44 +0200

    fix: type

--- api/v1/endpoints/fbt.py
+++ api/v1/endpoints/fbt.py
@@ -18,5 +18,5 @@ def get_db():
 def get_items(item_id_seed: int, db: Session = Depends(get_db)):
     items = crud.get_items(db, item_id_seed=item_id_seed)
     if items is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="No recommendations found")
     return [i.item for i in items]
