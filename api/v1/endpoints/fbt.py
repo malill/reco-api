6299a87ffe6c3ea193c02a761c424d1fd87af930
malill <markus.lill@gmx.de>
2021-09-06 15:06:37 +0200

    feat: return item object

--- api/v1/endpoints/fbt.py
+++ api/v1/endpoints/fbt.py
@@ -19,4 +19,4 @@ def get_items(item_id_seed: int, db: Session = Depends(get_db)):
     items = crud.get_items(db, item_id_seed=item_id_seed)
     if items is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return items
+    return [i.item for i in items]
