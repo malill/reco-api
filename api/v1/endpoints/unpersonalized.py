9a06aa525e6e30464f61fc1bd998e4b6f6a1457c
malill <markus.lill@gmx.de>
2021-09-09 14:48:13 +0200

    feat: add schemas.py

--- api/v1/endpoints/unpersonalized.py
+++ api/v1/endpoints/unpersonalized.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from core.db.database import SessionLocal
 from sqlalchemy.orm import Session
-from core.db import crud
+from core.db import crud, schemas
 
 router = APIRouter()
 
@@ -14,12 +16,12 @@ def get_db():
         db.close()
 
 
-@router.get("/unpersonalized/random")
+@router.get("/unpersonalized/random", response_model=List[schemas.Item])
 def get_random_items(db: Session = Depends(get_db)):
     return crud.get_random_items(db)
 
 
-@router.get("/unpersonalized/fbt/{item_id_seed}")
+@router.get("/unpersonalized/fbt/{item_id_seed}", response_model=List[schemas.Item])
 def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depends(get_db)):
     items = crud.get_frequently_bought_together_items(db, item_id_seed=item_id_seed)
     if items is None:
---
+++ core/db/schemas.py
@@ -0,0 +1,22 @@
+import logging
+from pydantic import BaseModel, validator
+
+
+class ItemBase(BaseModel):
+    name: str
+    price: str  # force two decimals
+
+    @validator('price')
+    def check_price(cls, price):
+        try:
+            return ("%.2f" % float(price)).replace('.', ',')
+        except ValueError:
+            logging.warning(f"'{price}' can not be transformed into price schema, using db response")
+            return price
+
+
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
