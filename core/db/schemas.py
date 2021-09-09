cf8b43a886ffb6dc427ecf4d6d412c8c835a9a3d
malill <markus.lill@gmx.de>
2021-09-09 16:10:32 +0200

    feat: set placeholder urls

--- core/db/schemas.py
+++ core/db/schemas.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel, validator
 class ItemBase(BaseModel):
     name: str
     price: str  # force two decimals
+    # todo: replace product placeholder url
+    product_url = "#"
+    # todo: replace image placeholder url
+    image_url = "https://img.fcbayern.com/image/upload/q_auto,f_auto/h_300," \
+                "c_pad/eCommerce/produkte/28757/t-shirt-upamecano.jpg "
 
     @validator('price')
     def check_price(cls, price):
