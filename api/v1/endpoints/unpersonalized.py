ecad4d94780bc6a95409dce26f6bace4caec17be
malill <markus.lill@gmx.de>
2021-10-26 12:21:54 +0200

    feat: add n_recos to endpoint

--- api/v1/endpoints/unpersonalized.py
+++ api/v1/endpoints/unpersonalized.py
@@ -17,13 +17,13 @@ def get_db():
 
 
 @router.get("/unpersonalized/random", response_model=List[schemas.Item])
-def get_random_items(db: Session = Depends(get_db)):
-    return crud.get_random_items(db)
+def get_random_items(db: Session = Depends(get_db), n_recos: int = 5):
+    return crud.get_random_items(db, n_recos)
 
 
 @router.get("/unpersonalized/fbt/{item_id_seed}", response_model=List[schemas.Item])
-def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depends(get_db)):
-    items = crud.get_frequently_bought_together_items(db, item_id_seed=item_id_seed)
+def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depends(get_db), n_recos: int = 5):
+    items = crud.get_frequently_bought_together_items(db, item_id_seed=item_id_seed, n_recos=n_recos)
     if items is None:
         raise HTTPException(status_code=404, detail="No recommendations found")
     return [i.item for i in items]
