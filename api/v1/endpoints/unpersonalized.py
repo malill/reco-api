9b443259f2d59ab6d6fe3a060b0c741e6b6d53fd
malill <markus.lill@gmx.de>
2021-10-26 12:35:40 +0200

    feat: refactor

--- api/v1/endpoints/unpersonalized.py
+++ api/v1/endpoints/unpersonalized.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from core.db.database import SessionLocal
 from sqlalchemy.orm import Session
 from core.db import crud, schemas
+from core.db.models import Item
 
 router = APIRouter()
 
@@ -17,12 +18,32 @@ def get_db():
 
 
 @router.get("/unpersonalized/random", response_model=List[schemas.Item])
-def get_random_items(db: Session = Depends(get_db), n_recos: int = 5):
+def get_random_items(db: Session = Depends(get_db), n_recos: int = 5) -> List[Item]:
+    """Return list of random items.
+
+    Args:
+        db (Session): Session object used for retrieving items from db.
+        n_recos (int): Number of items that should be returned.
+
+    Returns:
+        List[Item]: List of random items.
+    """
     return crud.get_random_items(db, n_recos)
 
 
 @router.get("/unpersonalized/fbt/{item_id_seed}", response_model=List[schemas.Item])
-def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depends(get_db), n_recos: int = 5):
+def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depends(get_db),
+                                         n_recos: int = 5) -> List[Item]:
+    """Return list of frequently bought together items given a seed item ID.
+
+    Args:
+        item_id_seed (int): ID of seed item that is used for finding frequently bought together items.
+        db (Session): Session object used for retrieving items from db.
+        n_recos (int): Number of items that should be returned.
+
+    Returns:
+        List[Item]: List of frequently bought together items.
+    """
     items = crud.get_frequently_bought_together_items(db, item_id_seed=item_id_seed, n_recos=n_recos)
     if items is None:
         raise HTTPException(status_code=404, detail="No recommendations found")
--- core/db/crud.py
+++ core/db/crud.py
@@ -1,13 +1,36 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 from core.db import models
 import random
 
+from core.db.models import Item
 
-def get_random_items(db: Session, n_recos=5):
+
+def get_random_items(db: Session, n_recos=5) -> List[Item]:
+    """Retrieve random items from 'content' db.
+
+    Args:
+        db (Session): Session object used for retrieving items from db.
+        n_recos (int): Number of items that should be returned.
+
+    Returns:
+        List[Item]: List of random items.
+    """
     rand = random.sample(range(0, db.query(models.Item).count()), n_recos)
     items = db.query(models.Item)
     return list(items[i] for i in rand)
 
 
-def get_frequently_bought_together_items(db: Session, item_id_seed: int, n_recos=5):
+def get_frequently_bought_together_items(db: Session, item_id_seed: int, n_recos=5) -> List[Item]:
+    """Retrieve frequently bought together items from 'recs' db.
+
+    Args:
+        db (Session): Session object used for retrieving items from db.
+        item_id_seed (int): ID of seed item that is used for finding frequently bought together items.
+        n_recos (int): Number of items that should be returned.
+
+    Returns:
+        List[Item]: List of frequently bought together items.
+    """
     return db.query(models.FBT).filter(models.FBT.item_id_seed == item_id_seed).limit(n_recos).all()
