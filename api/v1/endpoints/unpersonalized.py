bf38a79584137d49cb937a3e43b6d0713b0c58f1
malill <markus.lill@gmx.de>
2021-11-02 13:54:12 +0100

    fix: catch none item excepton

--- api/v1/endpoints/unpersonalized.py
+++ api/v1/endpoints/unpersonalized.py
@@ -47,4 +47,5 @@ def get_frequently_bought_together_items(item_id_seed: int, db: Session = Depend
     items = crud.get_frequently_bought_together_items(db, item_id_seed=item_id_seed, n_recos=n_recos)
     if items is None:
         raise HTTPException(status_code=404, detail="No recommendations found")
-    return [i.item for i in items]
+    # TODO: if clause here may cause endpoint return less than n_recos items -> should be moved to crud module
+    return [i.item for i in items if i.item is not None]
