35c355e0fee111d2a7459285948fa1a2075c0ced
malill <markus.lill@gmx.de>
2022-01-17 00:28:49 +0100

    feat: create custom fun for consumable

--- api/core/services/main.py
+++ api/core/services/main.py
@@ -1,5 +1,6 @@
 import logging
 import random
+from typing import List
 
 import pymongo
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -56,21 +57,23 @@ async def get_item_based_collaborative_filtering_items(conn: AsyncIOMotorClient,
     cursor = conn[cfg.DB_NAME_RECS][cfg.COLLECTION_NAME_COLLABORATIVE_FILTERING] \
         .find({'consumable_id_seed': item_id_seed}, {'_id': False}) \
         .sort([('similarity', pymongo.DESCENDING)])
-    sorted_el = await cursor.to_list(None)  # no limit, since we need to retrieve all
+    sorted_el = await cursor.to_list(None)  # no limit, since we need to retrieve all to get highest similarities
     ids = [str(e['consumable_id_recommended']) for e in sorted_el][:n_recos]
-    print(ids)
+    consumables = await get_consumables(conn, ids)
+    return consumables
 
+
+async def get_consumables(conn: AsyncIOMotorClient, ids: List[str]):
+    """Returns consumable objects in same order like parameter ids."""
     # TODO: use aggregate function to join collections
-
     cursor = conn[cfg.DB_NAME_EVIDENCE][cfg.COLLECTION_NAME_CONSUMABLE].find({'_id': {'$in': ids}})
     res = await cursor.to_list(None)
-
+    r = []
     for i in ids:
         for e in res:
             if e['_id'] == i:
-                print(e)
-
-    return res
+                r.append(e)
+    return r
 
 
 def quick_fix_adjust_item_id(item_id: int):
