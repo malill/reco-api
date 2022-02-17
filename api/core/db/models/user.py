fe95226910a70e9354de804de31f738cae6b44c7
malill <markus.lill@gmx.de>
2022-02-17 13:18:16 +0100

    feat: add names to user model

--- api/core/db/models/user.py
+++ api/core/db/models/user.py
@@ -1,9 +1,13 @@
+from typing import Optional
+
 from bson import ObjectId
 from pydantic import BaseModel, Field, Extra
 
 
 class BasicUserModel(BaseModel):
     keys: dict = Field()
+    first_name: Optional[str]
+    last_name: Optional[str]
 
     class Config:
         allow_population_by_field_name = True
@@ -12,6 +16,8 @@ class BasicUserModel(BaseModel):
         extra = Extra.allow
         schema_extra = {
             "example": {
+                "first_name": "Jane",
+                "last_name": "Doe",
                 "keys": {
                     'cookie': ['cookie_a', 'cookie_b'],
                     'canvas': ['canvas_a', 'canvas_b'],
