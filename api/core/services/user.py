8ddb078acfeafe94eba823c8c6af49cffd1fd1aa
malill <markus.lill@gmx.de>
2022-02-18 16:43:30 +0100

    feat: get user by cookie value

--- api/core/services/user.py
+++ api/core/services/user.py
@@ -9,16 +9,22 @@ from api.core.db.models.user import BasicUserModel
 import api.core.util.config as cfg
 
 
-async def get_user(conn: AsyncIOMotorClient, keys: dict):
-    """Probabilistic fetch method for user based on key values."""
-    # TODO: implement service
-    print(keys)
-    return "Hello User"
+def get_user_collection(conn: AsyncIOMotorClient):
+    return conn[cfg.DB_NAME][cfg.COLLECTION_NAME_USER]
+
+
+async def get_user(conn: AsyncIOMotorClient, cookie_key) -> BasicUserModel:
+    """Non-probabilistic fetch method for user based on cookie value."""
+    cursor = get_user_collection(conn).find({'keys.cookie': cookie_key})
+    res = await cursor.to_list(None)
+    if len(res) > 1:
+        print(f"Found more than one user for cookie_value: {cookie_key}")
+    return res[0]
 
 
 async def create_or_update_users(conn: AsyncIOMotorClient, user_models: List[BasicUserModel]):
     """Create or update user in db."""
     # TODO: need to find existing user by 'keys' attribute(s) of user
-    t = conn[cfg.DB_NAME][cfg.COLLECTION_NAME_USER]
-    await t.insert_many([jsonable_encoder(e, exclude_none=True) for e in user_models])
+    await get_user_collection(conn). \
+        insert_many([jsonable_encoder(e, exclude_none=True) for e in user_models])
     return JSONResponse(status_code=status.HTTP_201_CREATED)
--- api/v1/endpoints/collection/user.py
+++ api/v1/endpoints/collection/user.py
@@ -1,4 +1,5 @@
-from typing import List
+import string
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends, Body
 import api.core.services.user as service_user
@@ -10,6 +11,13 @@ from api.core.db.mongodb import get_database
 from api.core.util.config import ENDPOINT_COLLECTION, ENDPOINT_USER, TAG_USER
 
 api_router = APIRouter(prefix=ENDPOINT_COLLECTION + ENDPOINT_USER, tags=[TAG_USER])
+
+
+@api_router.get("", response_model=BasicUserModel)
+async def get_user(db: AsyncIOMotorClient = Depends(get_database), cookie_key: Optional[str] = None):
+    """Returns most probabilistic user matching keys."""
+    # TODO: current service get_user returns deterministic user by cookie value
+    return await service_user.get_user(db, cookie_key)
 
 
 @api_router.post("")
